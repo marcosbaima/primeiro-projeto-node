0219e3e1d0cad200a11530fc3fa872cd0b05f9d8
marcosbaima <marcosbaimace@gmail.com>
2020-05-26 19:22:05 -0300

    submmit_create_avatar

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticaated from '../middlewares/ensureAuthenticate';
 // Rota: Receberuma requisição, chamar outro arquivo, devolver uma resposta
 // SoC:
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -25,5 +30,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticaated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
