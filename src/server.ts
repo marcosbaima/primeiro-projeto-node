8a9bd54fdf0268a8ae93ceec5c1976fdfa7a01cc
marcosbaima <marcosbaimace@gmail.com>
2020-05-27 17:23:32 -0300

    commit_global_errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata';
-import express from 'express';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -11,6 +15,17 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.diretory));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: 'err.message',
+    });
+  }
+
+  console.error(err);
+});
 
 app.listen(3333, () => {
   console.log('o(*￣▽￣*)ブ Server started on port 3333');
